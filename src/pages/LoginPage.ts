97666e42923041d60d5f188b414eb97d83a89156
Sang Nguyen <ngcongsang@gmail.com>
2021-07-12 13:19:37 +0700

    rewrite playwright

--- src/pages/LoginPage.ts
+++ src/pages/LoginPage.ts
@@ -1,27 +1,23 @@
 import {Page} from 'playwright';
 
 export class LoginPage {
+  readonly page: Page;
+
   constructor(page: Page) {
     this.page = page;
   }
 
-  private page: Page;
-  private usernameInput = '#username';
-  private passwordInput = '#password';
-  private loginButton = "i[class='fa fa-2x fa-sign-in']";
+  private usernameInput = `input[name='username']`;
+  private passwordInput = `input[name='password']`;
+  private loginButton = `button[type='submit']`;
 
-  async navigate() {
-    await this.page.goto('https://the-internet.herokuapp.com/login', {
-      timeout: 50000,
-      waitUntil: 'networkidle'
-    });
+  async goTo() {
+    await this.page.goto('https://app.gridly.com/signin');
   }
 
   async signIn(userName: string, password: string) {
-    await this.page.waitForSelector(this.usernameInput);
     await this.page.type(this.usernameInput, userName);
     await this.page.type(this.passwordInput, password);
-    // await this.page.keyboard.press('Enter');
     await this.page.click(this.loginButton);
   }
 }
